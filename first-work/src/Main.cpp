4909fe8df400649c520a32510741f59ad39a87b4
Maria Eduarda <mariaeduardaeloi@outlook.com>
2021-12-11 21:03:29 -0300

    feat: read files of matrix

--- first-work/src/Main.cpp
+++ first-work/src/Main.cpp
@@ -7,35 +7,95 @@ using std::isdigit;
 #include <fstream>
 #include <string>
 #include <math.h>
-
+#include <sstream>
 
 
 int main(int argc, char* argv[])
 {
-     if(argc < 2  ||  !(std::isdigit(*argv[1])) )
-     {
-        cerr << "Por favor, informe os parâmetros corretamente" << endl;
+    if( argc < 3  ||  !(std::isdigit(*argv[1])) )
+    {
+        cerr << "Por favor, informe os parâmetros corretamente." << endl;
+        return 1;
+    }
+      
+    int orderMatrix = std::atoi(argv[1]);
+    char solution = *argv[2];
+
+    // Verificar se é uma potência de 2
+    float log = log2(orderMatrix);
+    if( (log == 0) || (log - int(log) != 0) )
+    {
+        cerr <<  orderMatrix << " não é uma potência de 2. Tente novamente." << endl;
+        return 1;
+    }
+    else if( solution != 'S' && solution != 'C' )
+    {
+        cerr << solution << " não é uma opção válida. Tente novamente." << endl;
         return 1;
     }
 
-    //verifique se é uma potência de 2
-    cout << log2(std::atoi(argv[1])) <<endl;
-    /**std::string orderMatrix = argv[1];
-    std::string  name_file = "../matrizes/A"+orderMatrix+"x"+orderMatrix+".txt";
-    std::ifstream inputFile(name_file, std::ios::in);
-    if(!inputFile.is_open()){
-        cerr<<"Erro ao ler arquivo"<<endl;
+    // Variáveis auxiliares para leitura das matrizes
+    int M, N; // dimensões
+    std::string line;
+    std::string name_file;
+    std::stringstream stream;
+
+    // Abrir arquivo da matriz A
+    name_file = "../matrizes/A"+std::to_string(orderMatrix)+"x"+std::to_string(orderMatrix)+".txt";
+    std::ifstream inputFileA(name_file, std::ios::in);
+    if(!inputFileA.is_open()){
+        cerr << "Erro ao ler arquivo: " << name_file << endl;
 		return 1;
 	}
 
-	std::string line;
-	while (!inputFile.eof()) {
-    	std::getline(inputFile, line);
-    	if (line.size() == 0) {
-      		continue;
-    	}
+    // Ler dimensões da matriz A
+    std::getline(inputFileA, line);
+    stream << line;
+    stream >> M >> N;
 
-    	cout << line << endl;
-  	}**/
+    int matrixA[M][N];
+    // Ler matriz A
+    for(int i=0; i<M; i++) {
+        std::getline(inputFileA, line);
+
+        stream.clear();
+        stream << line;
+
+        for(int j=0; j<N; j++)
+            stream >> matrixA[i][j];
+    }
+    inputFileA.close();
+
+
+    // Abrir arquivo da matriz B
+    name_file = "../matrizes/B"+std::to_string(orderMatrix)+"x"+std::to_string(orderMatrix)+".txt";
+    std::ifstream inputFileB(name_file, std::ios::in);
+    if(!inputFileB.is_open()){
+        cerr << "Erro ao ler arquivo: " << name_file << endl;
+        return 1;
+    }
+
+    // Ler dimensões da matriz B
+    std::getline(inputFileB, line);
+    stream.clear();
+    stream << line;
+    stream >> M >> N;
+    
+    int matrixB[M][N];
+    // Ler matriz B
+    for(int i=0; i<M; i++) {
+        std::getline(inputFileB, line);
+        
+        stream.clear();
+        stream << line;
+
+        for(int j=0; j<N; j++)
+            stream >> matrixB[i][j];
+    }
+    inputFileB.close();
+
+
+    // 
+
     return 0;
 }
